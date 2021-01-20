aabb9c1e48d3b47dd68556e251b9f5d991e2b70a
LysitheaStar <lysithea.star@gmail.com>
2021-01-20 16:34:56 +0300

    UI & new functions

--- CloseActPlugin/CloseActPlugin.cs
+++ CloseActPlugin/CloseActPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
@@ -12,23 +13,99 @@ namespace CloseActPlugin
     {
         private Label lblStatus;   
         private Thread CloseWorkerThread;
+        public SettingsPage SettingsCtr = null;
+        private bool isCombatEventRegistered = false;
+        private bool isCloseThreadStarted = false;
+        private readonly string SingletonFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\CloseActPlugin.singleton");
+        private FileStream SingletonFS = null;
+        public Boolean Initialized = false;
 
         public void DeInitPlugin()
         {
-            CloseWorkerThread.Abort();
-            ActGlobals.oFormActMain.OnCombatEnd -= CombatEnded;
+            StopCloseTask();
+            UnregisterClearEvent();
             UpdateText("Plugin Exited");
+            Initialized = false;
         }
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
         {
-            //((TabControl)pluginScreenSpace.Parent).TabPages.Remove(pluginScreenSpace);
+            if (SettingsCtr == null) SettingsCtr = new SettingsPage(this);
+            pluginScreenSpace.Controls.Add(SettingsCtr);
             lblStatus = pluginStatusText;
+            Settings.ReadFromFile(this);
+            if (Settings.GetMinimizeEnabled()) ActGlobals.oFormActMain.WindowState = FormWindowState.Minimized;
+            SettingsUpdated();
             UpdateText("Plugin Started");
-            CloseWorker w = new CloseWorker(this);
-            CloseWorkerThread = new Thread(w.Main);
-            CloseWorkerThread.Start();
-            ActGlobals.oFormActMain.WindowState = FormWindowState.Minimized;
-            ActGlobals.oFormActMain.OnCombatEnd += CombatEnded;
+            Initialized = true;
+        }
+
+        public void StartCloseTask()
+        {
+            if (!isCloseThreadStarted) 
+            { 
+                CloseWorker w = new CloseWorker(this);
+                CloseWorkerThread = new Thread(w.Main);
+                CloseWorkerThread.Start();
+                isCloseThreadStarted = true;
+            }
+        }
+
+        public void StopCloseTask()
+        {
+            if (isCloseThreadStarted)
+            {
+                CloseWorkerThread.Abort();
+                isCloseThreadStarted = false;
+            }
+        }
+
+        public void RegisterClearEvent()
+        {
+            if (!isCombatEventRegistered)
+            {
+                ActGlobals.oFormActMain.OnCombatEnd += CombatEnded;
+                isCombatEventRegistered = true;
+            }
+        }
+
+        public void UnregisterClearEvent()
+        {
+            if (isCombatEventRegistered)
+            {
+                ActGlobals.oFormActMain.OnCombatEnd -= CombatEnded;
+                isCombatEventRegistered = false;
+            }
+        }
+
+        public void ObtainSingletonLock()
+        {
+            try
+            {
+                if (SingletonFS == null) SingletonFS = new FileStream(SingletonFile, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("You have set CloseActPlugin to prevent running of 2nd copy of ACT. This instance will now close."
+                    , "Another ACT instance is running", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ActGlobals.oFormActMain.Close();
+            }
+        }
+
+        public void ReleaseSingletonLock()
+        {
+            if (SingletonFS != null) 
+            { 
+                SingletonFS.Close();
+                SingletonFS = null;
+            }
+        }
+
+        public void SettingsUpdated()
+        {
+            Settings.ReadSettingsFromTab(this);
+            if (Settings.GetCloseEnabled()) StartCloseTask(); else StopCloseTask();
+            if (Settings.GetClearEnabled()) RegisterClearEvent(); else UnregisterClearEvent();
+            if (Settings.GetSingletonEnabled()) ObtainSingletonLock(); else ReleaseSingletonLock();
         }
         
         public void UpdateText(String txt)
---
+++ CloseActPlugin/Settings.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Advanced_Combat_Tracker;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CloseActPlugin
+{
+    public class Settings
+    {
+        public static bool[] cfg = new bool[] { true, true, false, false };
+
+        public static bool GetMinimizeEnabled()
+        {
+            return cfg[0];
+        }
+        public static bool GetCloseEnabled()
+        {
+            return cfg[1];
+        }
+        public static bool GetClearEnabled()
+        {
+            return cfg[2];
+        }
+        public static bool GetSingletonEnabled()
+        {
+            return cfg[3];
+        }
+
+        public static void ReadSettingsFromTab(CloseActPluginMain p)
+        {
+            cfg = new bool[]
+            {
+                p.SettingsCtr.ChkMinimize.Checked,
+                p.SettingsCtr.ChkClose.Checked,
+                p.SettingsCtr.ChkClear.Checked,
+                p.SettingsCtr.ChkSingleton.Checked
+            };
+        }
+
+        public static void ReadFromFile(CloseActPluginMain p)
+        {
+            String settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\CloseActPlugin.dat");
+            try
+            {
+                String[] s = File.ReadAllText(settingsFile).Split(',');
+                for (int i = 0; i < s.Length; i++)
+                {
+                    cfg[i] = Boolean.Parse(s[i]);
+                }
+            }
+            catch (Exception) { }
+            p.SettingsCtr.ChkMinimize.Checked = GetMinimizeEnabled();
+            p.SettingsCtr.ChkClose.Checked = GetCloseEnabled();
+            p.SettingsCtr.ChkClear.Checked = GetClearEnabled();
+            p.SettingsCtr.ChkSingleton.Checked = GetSingletonEnabled();
+        }
+
+        public static void SaveToFile(CloseActPluginMain p)
+        {
+            String settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\CloseActPlugin.dat");
+            try
+            {
+                String s = String.Join(",", cfg);
+                File.WriteAllText(settingsFile, s);
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message, "Error occurred during attempt to save settings", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
